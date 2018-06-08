cea5841ab7736536702dd78c7da321e715057b85
conamatic <connor.thursfield@weightroncb.co.uk>
2018-06-08 08:16:06 +0100

    Added exe path to process start arguments

--- JovoUpdater/Program.cs
+++ JovoUpdater/Program.cs
@@ -1,64 +1,85 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Newtonsoft.Json;
-
 
 namespace JovoUpdater
 {
     class Program
     {
         static Jovo Config;
-
         static void Main(string[] args)
         {
-
-            Config = JsonConvert.DeserializeObject<Jovo>(File.ReadAllText("manifest.json"));
-            if (!Config.UpdateFromPath.EndsWith("\\"))
-                Config.UpdateFromPath += "\\";
-            if (!Config.InstalledPath.EndsWith("\\"))
-                Config.InstalledPath += "\\";
-
-            if (!Directory.Exists(Config.InstalledPath))
-                Directory.CreateDirectory(Config.InstalledPath);
-
-            if (File.Exists(Config.InstalledPath + "\\manifest.json"))
+            try
             {
-                Jovo Installed = JsonConvert.DeserializeObject<Jovo>(File.ReadAllText(Config.InstalledPath + "manifest.json"));
-
-                if (File.Exists(Config.UpdateFromPath + "\\manifest.json"))
-                {
-                    Jovo Server = JsonConvert.DeserializeObject<Jovo>(File.ReadAllText(Config.UpdateFromPath + "\\manifest.json"));
-
-                    if (new Version(Installed.Version) < new Version(Server.Version))
-                    {
-                        File.Copy(Config.UpdateFromPath + "Jovo.exe", Config.InstalledPath + "Jovo.exe", true);
-                        File.Copy(Config.UpdateFromPath + "manifest.json", Config.InstalledPath + "manifest.json", true);
-                    }
-                }
-            }
-            else
+                Config = JsonConvert.DeserializeObject<Jovo>(File.ReadAllText("manifest.json"));
+                if (!Config.UpdateFromPath.EndsWith("\\"))
+                    Config.UpdateFromPath += "\\";
+                if (!Config.InstalledPath.EndsWith("\\"))
+                    Config.InstalledPath += "\\";
+                //Console.WriteLine("Got configuration manifest");
+            } catch (Exception ex_a)
             {
-                if (File.Exists(Config.UpdateFromPath + "Jovo.exe") && File.Exists(Config.UpdateFromPath + "manifest.json"))
-                {
-                    string[] allFiles = Directory.GetFiles(Config.UpdateFromPath);
-                    foreach (string file in allFiles)
-                    {
-                        File.Copy(Config.UpdateFromPath + file.Split('\\').Last(), Config.InstalledPath + file.Split('\\').Last());
-                    }
-                }
+                //Console.WriteLine(ex_a.ToString());
             }
 
             try
             {
-                Process.Start(Config.InstalledPath + "Jovo.exe");
+                if (!Directory.Exists(Config.InstalledPath))
+                    Directory.CreateDirectory(Config.InstalledPath);
+            } catch (Exception ex_b)
+            {
+                //Console.WriteLine(ex_b);
             }
-            catch (Exception) { }
 
+            try
+            {
+                if (File.Exists(Config.InstalledPath + "manifest.json") && File.Exists(Config.UpdateFromPath + "manifest.json"))
+                {
+                    Jovo Installed = JsonConvert.DeserializeObject<Jovo>(File.ReadAllText(Config.InstalledPath + "manifest.json"));
+                    Jovo Server = JsonConvert.DeserializeObject<Jovo>(File.ReadAllText(Config.UpdateFromPath + "manifest.json"));
+                    //Console.WriteLine("Got installed & server manifests");
+
+                    if (new Version(Installed.Version) < new Version(Server.Version))
+                    {
+                        //Console.WriteLine("Trying to update...");
+                        File.Copy(Config.UpdateFromPath + "Jovo.exe", Config.InstalledPath + "Jovo.exe", true);
+                        File.Copy(Config.UpdateFromPath + "manifest.json", Config.InstalledPath + "manifest.json", true);
+                        //Console.Write("Updated");
+                    } else
+                    {
+                        //Console.WriteLine("Didn't update (up to date)");
+                    }
+                }
+                else
+                {
+                    if (File.Exists(Config.UpdateFromPath + "Jovo.exe") && File.Exists(Config.UpdateFromPath + "manifest.json"))
+                    {
+                        //Console.WriteLine("Installing for the first time");
+                        string[] allFiles = Directory.GetFiles(Config.UpdateFromPath);
+                        foreach (string file in allFiles)
+                        {
+                            File.Copy(Config.UpdateFromPath + file.Split('\\').Last(), Config.InstalledPath + file.Split('\\').Last());
+                            //Console.WriteLine("Copied " + file);
+                        }
+                    }
+                }
+            } catch (Exception ex_c)
+            {
+                //Console.WriteLine(ex_c.ToString());
+            }
+            //Console.WriteLine("Ready to start");
+
+            ProcessStartInfo startInfo = new ProcessStartInfo
+            {
+                WorkingDirectory = Config.InstalledPath,
+                FileName = "Jovo.exe",
+                Arguments = System.Reflection.Assembly.GetExecutingAssembly().Location
+            };
+
+            Directory.SetCurrentDirectory(Config.InstalledPath);
+            Process Jovo = Process.Start(startInfo);
         }
     }
 
